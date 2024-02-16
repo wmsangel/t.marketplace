614287d82cc5ed7a11fba9349005a61237c86eff
Igor N. Zagorodnyi <wmsangel@gmail.com>
2024-02-16 10:25:29 +0600

    Меняем JS имена файлов на нормальные

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 $(function (){
     // Range block
     if ($('.js-noui').length) {
-        $.getScript("assets/js/_range.js", function () {});
+        $.getScript("assets/js/range.js", function () {});
     }
 
     // Nose block
     if ($('.js-nose-radio').length) {
-        $.getScript("assets/js/_nose.js", function () {});
+        $.getScript("assets/js/nose.js", function () {});
     }
 
     // Filters block
     if ($('.js-filter-btn').length) {
-        $.getScript("assets/js/_filters.js", function () {});
+        $.getScript("assets/js/filters.js", function () {});
     }
 })
